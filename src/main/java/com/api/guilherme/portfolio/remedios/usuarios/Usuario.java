b7d00b4deb49883ea7bad9e940b3048ed23ae99a
Guilherme Alves de Carvalho <113042890+guiAlvesCarvalho@users.noreply.github.com>
2023-08-01 22:55:52 -0300

    Usuario, UsuarioRepository, AutenticaçãoService e SecurityConfigurations

---
+++ src/main/java/com/api/guilherme/portfolio/remedios/usuarios/Usuario.java
@@ -0,0 +1,21 @@
+package com.api.guilherme.portfolio.remedios.usuarios;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "Usuarios")
+@Entity(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
